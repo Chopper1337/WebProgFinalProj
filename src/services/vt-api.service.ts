4313ba56e3117f79e63032053e9ec2dcfd62f2a9
joseph <joseph@X6>
2022-05-05 08:56:00 +0100

    VT API service most done (afaik)

--- src/services/vt-api.service.ts
+++ src/services/vt-api.service.ts
@@ -1,23 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse} from "@angular/common/http";
+import { HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import { VTAPIResponse } from 'src/app/vt-api-response';
+import { Observable, throwError } from 'rxjs';
+import { catchError,tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VtApiService {
 
-  private _siteURL="https://www.virustotal.com/vtapi/v2/url/scan";
-  private _ApiKey="0043c4b8d033dd717ac40bc4178a9887048e43cd8f7241ad538671ae7cd9c3ab"; // yes I am aware this is public :)
+  private _siteURL="https://www.virustotal.com/api/v3/urls/"; // Append the base64 encoded URL to this
+  private _ApiKey="0043c4b8d033dd717ac40bc4178a9887048e43cd8f7241ad538671ae7cd9c3ab"; // Header named "x-Apikey" + yes I am aware this is public :)
   constructor(private _http:HttpClient) { }
   
-  scanURL(string:URL){
+  // This is take the users inputted URL as a string, converts it to base64 and makes the API request to the 
+  scanURL(string:URL): Observable<VTAPIResponse>{
     /*
     console.log(Buffer.from("Hello World").toString('base64'));
     // SGVsbG8gV29ybGQ=
     console.log(Buffer.from("SGVsbG8gV29ybGQ=", 'base64').toString('binary'))
     // Hello World
 */
+  const customHeaders = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'x-Apikey': this._ApiKey
+  }
 
+  const requestOptions = {                                                                                                                                                                                 
+    headers: new HttpHeaders(customHeaders), 
+  }
 
+// Need to add the "x-ApiKey" header (idk how yet)
+  return this._http.get<VTAPIResponse>(this._siteURL + Buffer.from(URL).toString('base64'), requestOptions)
+    .pipe(
+      tap(data => console.log('VTAPIError: ' + JSON.stringify(data)))
+    )
   }
 }
