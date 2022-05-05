78dffa40b74467de0a9dabdda3249d9600f9c38e
joseph <joseph@X6>
2022-05-05 09:21:45 +0100

    Create getUrlScanReport method in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { stringify } from 'querystring';
+import { VtApiService } from 'src/services/vt-api.service';
+import { VTAPIResponse } from './vt-api-response';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+  title = 'URL Scanner';
+  
+  websiteData!:VTAPIResponse;
+  error:any;
+   
+  constructor(private _vtApiService:VtApiService){}
+  
+  getURLScanReport(WebsiteURL) : boolean{
+    this._vtApiService.scanURL(WebsiteURL).subscribe(
+     websiteData => {
+        this.websiteData=websiteData;
+      },
+      error => this.error = <any>error
+    );
+    return false;
+  }
 }
